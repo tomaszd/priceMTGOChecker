90ae1a1ceeb2b0b1b8f397e813cbf7a32fad0652
tomaszd <tomaszdudziec@gmail.com>
2014-02-18 08:33:12 +0100

    Update models.py
    changes to card model

--- priceMTGOChecker/prices/models.py
+++ priceMTGOChecker/prices/models.py
@@ -9,8 +9,13 @@ from django.db import models
 #     question_text = models.CharField(max_length=200,)
 #     pub_date = models.DateTimeField('date published')
 
+####
 
+
+#####
 class Color(models.Model):
+    COLORS=Choices('WHITE','BLUE','GREEN','RED',
+                   'BLACK','COLORLESS','MULTICOLOR')
     colors_choices=(('WHITE','w'),
                     ('BLUE','u'),
                     ('GREEN','g'),
@@ -19,3 +24,6 @@ class Color(models.Model):
                     ('COLORLESS','0'),
                     ('MULTICOLOR','mc'))
     color = models.CharField(max_length=20, choices=colors_choices)
+    chrome= models.CharField(
+        choices=COLORS, max_length=20,
+        default=COLORS.COLORLESS)
