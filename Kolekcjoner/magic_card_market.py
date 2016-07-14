19f56ba6677d4d5c37b4e7301f65a623642adec6
tomaszd <tomaszdudziec@gmail.com>
2016-07-14 22:43:28 +0200

    few fixups

--- Kolekcjoner/magic_card_market.py
+++ Kolekcjoner/magic_card_market.py
@@ -9,7 +9,6 @@ import requests
 
 def get_singles_details_MCM(cardname=None):
   url = "http://www.magiccardmarket.eu/?mainPage=showSearchResult&searchFor="
-
   if not cardname:
     print "no card specified"
     return []
@@ -27,11 +26,11 @@ def get_singles_details_MCM(cardname=None):
   base_url = "https://www.magiccardmarket.eu"
   for row in range(len(soup.findAll('tr')))[1:-1]:
     card_details = {}
-    # print "checking ",full_urlsfd
+    # print "checking ",full_url
     try :
       thumb, expan, rarity, href, comment, singles, avail, price = soup.findAll('tr')[row].findAll('td')
     except:
-      print "just one occurrence for {}".format(full_url)
+      print "just one occurence for {}".format(full_url)
       card_details = {
 
                   "href":   full_url,
@@ -40,8 +39,8 @@ def get_singles_details_MCM(cardname=None):
                   "avail": False,
                   "expansion_set":"strange",
                   "price": "strange"}
+      print "dupa2"
       return [ card_details ]
-
     if price.text == "N/A":
       print "Warning! N/A for price {} .{}".format(price, href)
       continue
@@ -51,16 +50,14 @@ def get_singles_details_MCM(cardname=None):
     if "Promo" in str(href) :
       print "Warning! This is PROMO price , Skipping...", href
       continue
+
     thumb = str(thumb).replace('<td>', '').replace('</td>', '')
     expan = str(expan).replace('<td>', '').replace('</td>', '')
     href = str(href).replace('<td>', '').replace('</td>', '')
     comment = str(comment).replace('<td>', '').replace('</td>', '')
     singles = str(singles).replace('<td>', '').replace('</td>', '')
     avail = str(avail).replace('<td>', '').replace('</td>', '')
-
-    price = float(str(price).replace('<td>', '').replace('</td>', '').replace(
-                      '&#x20AC;', '').replace(
-                      ',', '.').replace(' ', ''))
+    price = str(price).replace('<td>', '').replace('</td>', '').replace('&#x20AC;', 'EURO')
     is_card_single = 'Products/Singles/' in href
     if is_card_single:
       expansion_set = href.split('Singles/')[1].split('/')[0]
@@ -71,9 +68,19 @@ def get_singles_details_MCM(cardname=None):
       print "Expansion set is {} .skipping then ...for {}".format(expansion_set,
                                                                   cardname)
       continue
-
+    if "WCD" in expansion_set :
+      print "Expansion set is {} .skipping then ...HIS gold bordered {}".format(expansion_set,
+                                                                  cardname)
+      continue
+    # print "thumb", thumb
+    # print "expan", expan
+    # print "rarity", rarity
     if comment:
       print "comment", comment
+    # print "singles", singles
+    # print "avail", avail
+#    print "expansion_set", expansion_set
+#    print "price", price
 
     card_details = {
                   # "thumb":thumb,
@@ -88,22 +95,23 @@ def get_singles_details_MCM(cardname=None):
 
     list_of_cards.append(card_details)
   if list_of_cards == []	:
-    print "WARNING! . There are no cards"
+    print "Nie ma!"
   return list_of_cards
 
 def get_best_expansion(list_of_cards):
     """This is getting lowest price dict. the cheapest expansion set"""
   #   for single_detail in list_of_cards:
   #     print single_detail['expansion_set'] , " : ", single_detail['price']
-
-    newlist_sorted_by_price = sorted(list_of_cards, key=lambda k: k['price'])
+    newlist_sorted_by_price = sorted(list_of_cards,
+                                     key=lambda k: float(k['price'].
+                                     replace(",", ".").replace("EURO", "")))
   #    for single_detail in newlist_sorted_by_price:
   #      print single_detail['expansion_set'] , " : ", single_detail['price']
     lowest_price_single = newlist_sorted_by_price[0]
     return lowest_price_single
 
 def get_price_trend(lowest_price_single):
-  """get price tend form sites like :
+  """get price tend form sites like : 
    https://www.magiccardmarket.eu/Products/Singles/Future+Sight/Tarmogoyf"""
   # 'href': '<a href="/Products/Singles/Magic+2012/Manabarbs">Manabarbs</a>' -> /Products/Singles/Magic+2012/Manabarbs
   url_for_single = lowest_price_single["href"]
@@ -127,7 +135,7 @@ def get_price_trend(lowest_price_single):
 
 def get_price_and_set_MagicCardMarket(cardname):
   """ function return dict with all details about card in form:
-
+  
   {'comment': '',
    'singles': 'Singles',
    price_trend': u'113,50 EURO'
@@ -142,9 +150,7 @@ def get_price_and_set_MagicCardMarket(cardname):
 
 if __name__ == "__main__":\
   # print get_price_and_set_MagicCardMarket("tarmogoyf")
-  cardname = "Soul Majesty"
-  print get_singles_details_MCM(cardname)
-  print get_best_expansion(get_singles_details_MCM(cardname))
-  # print get_price_and_set_MagicCardMarket("Misty Rainforest")
-
+  # print get_price_and_set_MagicCardMarket("doran, the siege tower")
+  # print get_price_and_set_MagicCardMarket("flooded strand")
+  print get_price_and_set_MagicCardMarket(" Fire-lit thicket")
 
