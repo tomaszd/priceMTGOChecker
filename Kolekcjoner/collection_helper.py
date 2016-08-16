0811b0969e7d1831954ef7f5cd4fb6cc4c4722c0
tomaszd <tomaszdudziec@gmail.com>
2016-08-16 22:56:47 +0200

    autopep8

--- Kolekcjoner/collection_helper.py
+++ Kolekcjoner/collection_helper.py
@@ -5,96 +5,101 @@ import os
 import urllib
 import magic_card_market
 
-##########
+#
 # This file is for methods / class for counting amount of monet for collection
 # load collection xlsx
-def convert_xlsx2dict(input_file):
-  wb = openpyxl.load_workbook(input_file)
-  sheet = wb.get_sheet_by_name('Sheet1')
-  # transfom sheet ->dict
-  cards_dict = []
-  for i in range(1, len(sheet.rows)):
-    cards_dict.append(
-                  {
-                  'nazwa':sheet.rows[i][0].value,
-                  'ilosc':int(sheet.rows[i][1].value),
-                  'kolor':sheet.rows[i][2].value,
-                  'komentarz':sheet.rows[i][3].value
-                   }
-                 )
 
-  return cards_dict
+
+def convert_xlsx2dict(input_file):
+    wb = openpyxl.load_workbook(input_file)
+    sheet = wb.get_sheet_by_name('Sheet1')
+    # transfom sheet ->dict
+    cards_dict = []
+    for i in range(1, len(sheet.rows)):
+        cards_dict.append(
+            {
+                'nazwa': sheet.rows[i][0].value,
+                'ilosc': int(sheet.rows[i][1].value),
+                'kolor': sheet.rows[i][2].value,
+                'komentarz': sheet.rows[i][3].value
+            }
+        )
+
+    return cards_dict
+
 
 def getCFBPrice(cardName, cardSet=None):
-  """get card price from ChannelFireball cardname."""
-  cfbURL = "http://store.channelfireball.com/products/search?q=" + urllib.quote(cardName)
-  htmlFile = urllib.urlopen(cfbURL)
-  rawHTML = htmlFile.read()
-  tempIndex = rawHTML.find("grid-item-price")
-  startPriceIndex = rawHTML.find("$", tempIndex)
-  endPriceIndex = rawHTML.find("<", startPriceIndex)
-  cfbPrice = rawHTML[startPriceIndex:endPriceIndex]
-  print "{0}: {1}".format(cardName, cfbPrice)
-  return cfbPrice
+    """get card price from ChannelFireball cardname."""
+    cfbURL = "http://store.channelfireball.com/products/search?q=" + \
+        urllib.quote(cardName)
+    htmlFile = urllib.urlopen(cfbURL)
+    rawHTML = htmlFile.read()
+    tempIndex = rawHTML.find("grid-item-price")
+    startPriceIndex = rawHTML.find("$", tempIndex)
+    endPriceIndex = rawHTML.find("<", startPriceIndex)
+    cfbPrice = rawHTML[startPriceIndex:endPriceIndex]
+    print "{0}: {1}".format(cardName, cfbPrice)
+    return cfbPrice
+
 
 def getTCGPlayerPrices(cardName, cardSet=None):
-  """ Open the TCGPlayer URL and get cardname price"""
-  tcgPlayerURL = "http://magic.tcgplayer.com/db/magic_single_card.asp?cn=" + urllib.quote(cardName)
-  if cardSet:
-    tcgPlayerURL += "&sn=" + urllib.quote(cardSet)
-  htmlFile = urllib.urlopen(tcgPlayerURL)
-  rawHTML = htmlFile.read()
-  # Scrape for the low price
-  tempIndex = rawHTML.find('>Low:')
-  startLowIndex = rawHTML.find("$", tempIndex)
-  endLowIndex = rawHTML.find("<", startLowIndex)
-  lowPrice = rawHTML[startLowIndex:endLowIndex]
-  # Scrape for the mid price
-  tempIndex = rawHTML.find('>Median:')
-  startMidIndex = rawHTML.find("$", tempIndex)
-  endMidIndex = rawHTML.find("<", startMidIndex)
-  midPrice = rawHTML[startMidIndex:endMidIndex]
-  # Scrape for the high price
-  tempIndex = rawHTML.find('>High:')
-  startHighIndex = rawHTML.find("$", tempIndex)
-  endHighIndex = rawHTML.find("<", startHighIndex)
-  highPrice = rawHTML[startHighIndex:endHighIndex]
-  print [lowPrice, midPrice, highPrice]
-  # import pdb
-  # pdb.set_trace()
+    """ Open the TCGPlayer URL and get cardname price"""
+    tcgPlayerURL = "http://magic.tcgplayer.com/db/magic_single_card.asp?cn=" + \
+        urllib.quote(cardName)
+    if cardSet:
+        tcgPlayerURL += "&sn=" + urllib.quote(cardSet)
+    htmlFile = urllib.urlopen(tcgPlayerURL)
+    rawHTML = htmlFile.read()
+    # Scrape for the low price
+    tempIndex = rawHTML.find('>Low:')
+    startLowIndex = rawHTML.find("$", tempIndex)
+    endLowIndex = rawHTML.find("<", startLowIndex)
+    lowPrice = rawHTML[startLowIndex:endLowIndex]
+    # Scrape for the mid price
+    tempIndex = rawHTML.find('>Median:')
+    startMidIndex = rawHTML.find("$", tempIndex)
+    endMidIndex = rawHTML.find("<", startMidIndex)
+    midPrice = rawHTML[startMidIndex:endMidIndex]
+    # Scrape for the high price
+    tempIndex = rawHTML.find('>High:')
+    startHighIndex = rawHTML.find("$", tempIndex)
+    endHighIndex = rawHTML.find("<", startHighIndex)
+    highPrice = rawHTML[startHighIndex:endHighIndex]
+    print [lowPrice, midPrice, highPrice]
+    # import pdb
+    # pdb.set_trace()
 
-  return [lowPrice, midPrice, highPrice]
-
+    return [lowPrice, midPrice, highPrice]
 
 
 if __name__ == "__main__":
-  print 'Example Test'
-  input_file = 'Luty2016.xlsx'
-  # input_file = 'Test2016.xlsx'
-  my_cards = convert_xlsx2dict(input_file)
-  errors = []
-  total = 0
-  lazy_loading_dict = {}
-  for karta in my_cards:
-    if karta['nazwa'] in lazy_loading_dict.keys():
-      price = lazy_loading_dict[karta['nazwa']]
-      print "Already price was taken for {} it is {}".format(karta['nazwa'], price)
+    print 'Example Test'
+    input_file = 'Luty2016.xlsx'
+    # input_file = 'Test2016.xlsx'
+    my_cards = convert_xlsx2dict(input_file)
+    errors = []
+    total = 0
+    lazy_loading_dict = {}
+    for karta in my_cards:
+        if karta['nazwa'] in lazy_loading_dict.keys():
+            price = lazy_loading_dict[karta['nazwa']]
+            print "Already price was taken for {} it is {}".format(karta['nazwa'], price)
 
-    else:
-      price = "0"
-      try:
-        card_details = magic_card_market.get_price_and_set_MagicCardMarket(
-            karta['nazwa'])
-        price_trend = card_details["price_trend"].split(" ")[0]
-        expansion_set = card_details['expansion_set']
-        # .split( " " )[0] to remove " EURO"
-        price = card_details['price']
-        karta['expansion_set'] = expansion_set
-        karta['href'] = card_details["href"]
-        # #Na razie price liczone z prcie trendu
-        price = price_trend
-        # price = getCFBPrice( karta['nazwa'] )
-        """
+        else:
+            price = "0"
+            try:
+                card_details = magic_card_market.get_price_and_set_MagicCardMarket(
+                    karta['nazwa'])
+                price_trend = card_details["price_trend"].split(" ")[0]
+                expansion_set = card_details['expansion_set']
+                # .split( " " )[0] to remove " EURO"
+                price = card_details['price']
+                karta['expansion_set'] = expansion_set
+                karta['href'] = card_details["href"]
+                # Na razie price liczone z prcie trendu
+                price = price_trend
+                # price = getCFBPrice( karta['nazwa'] )
+                """
         {'comment': '',
         'singles': 'Singles',
         price_trend': u'113,50 EURO'
@@ -103,45 +108,46 @@ if __name__ == "__main__":
         'href': 'https://www.magiccardmarket.eu/Products/Singles/Magic+2012/Manabarbs',
         'expansion_set': 'Magic+2012'}
         """
-      except:
-        print "issues for {}".format(karta)
-        errors.append(karta)
+            except:
+                print "issues for {}".format(karta)
+                errors.append(karta)
 
-
-    karta['cena'] = price
-    lazy_loading_dict[karta['nazwa']] = price
-    total += karta['ilosc'] * float(price.replace('$', '').replace(",", "."))
-    print " after adding {} {} - worth total in EURO :  {} -> added {}".format(
-                                                                 karta['ilosc'],
-                                                                 price,
-                                                                 total,
-                                                                 karta['nazwa'])
-    """
+        karta['cena'] = price
+        lazy_loading_dict[karta['nazwa']] = price
+        total += karta['ilosc'] * float(
+            price.replace('$', '').replace(",", "."))
+        print " after adding {} {} - worth total in EURO :  {} -> added {}".format(
+            karta[
+                'ilosc'],
+            price,
+            total,
+            karta['nazwa'])
+        """
     except:
       print "issues for {}".format(karta)
       errors.append(karta)
       # karta['cenaTCG']=getTCGPlayerPrices(karta['nazwa'])
     """
 
-  timestamp = datetime.datetime.now().strftime("%Y_%B")
-  result_path = "Results"
-  if not os.path.exists(result_path):
-    print "There is no path {} . Creating it".format(result_path)
-    os.mkdir(result_path)
+    timestamp = datetime.datetime.now().strftime("%Y_%B")
+    result_path = "Results"
+    if not os.path.exists(result_path):
+        print "There is no path {} . Creating it".format(result_path)
+        os.mkdir(result_path)
 
-  detailed_result_path = os.path.join(result_path,
-                                      "Results_" + str(timestamp))
-  if not os.path.exists(detailed_result_path):
-    print "There is no path {} . Creating it".format(detailed_result_path)
-    os.mkdir(detailed_result_path)
+    detailed_result_path = os.path.join(result_path,
+                                        "Results_" + str(timestamp))
+    if not os.path.exists(detailed_result_path):
+        print "There is no path {} . Creating it".format(detailed_result_path)
+        os.mkdir(detailed_result_path)
 
-  with open(os.path.join(detailed_result_path,
-                         'result' + timestamp + '.txt'), 'w') as outfile:
-    json.dump(my_cards, outfile)
-  with open(os.path.join(detailed_result_path,
-                         'errors' + timestamp + '.txt'), 'w') as outfile2:
-    json.dump(errors, outfile2)
-  with open(os.path.join(detailed_result_path,
-                         'total' + timestamp + '.txt'), 'w') as outfile3:
-    json.dump(total, outfile3)
-  print "Finito!!!!!"
+    with open(os.path.join(detailed_result_path,
+                           'result' + timestamp + '.txt'), 'w') as outfile:
+        json.dump(my_cards, outfile)
+    with open(os.path.join(detailed_result_path,
+                           'errors' + timestamp + '.txt'), 'w') as outfile2:
+        json.dump(errors, outfile2)
+    with open(os.path.join(detailed_result_path,
+                           'total' + timestamp + '.txt'), 'w') as outfile3:
+        json.dump(total, outfile3)
+    print "Finito!!!!!"
--- Kolekcjoner/magic_card_market.py
+++ Kolekcjoner/magic_card_market.py
@@ -8,149 +8,154 @@ import requests
 
 
 def get_singles_details_MCM(cardname=None):
-  url = "http://www.magiccardmarket.eu/?mainPage=showSearchResult&searchFor="
-  if not cardname:
-    print "no card specified"
-    return []
+    url = "http://www.magiccardmarket.eu/?mainPage=showSearchResult&searchFor="
+    if not cardname:
+        print "no card specified"
+        return []
 
-  full_url = url + cardname
-  try:
-    data = requests.get(full_url)
-    soup = BeautifulSoup(data.text)
-  except:
-    print "problems for {}".format(full_url)
-    return []
-  list_of_cards = []
-  # [1:-1] cause first element is column legend names
-  # and the last is empty
-  base_url = "https://www.magiccardmarket.eu"
-  for row in range(len(soup.findAll('tr')))[1:-1]:
-    card_details = {}
-    # print "checking ",full_url
-    try :
-      thumb, expan, rarity, href, comment, singles, avail, price = soup.findAll('tr')[row].findAll('td')
+    full_url = url + cardname
+    try:
+        data = requests.get(full_url)
+        soup = BeautifulSoup(data.text)
     except:
-      print "just one occurence for {}".format(full_url)
-      card_details = {
+        print "problems for {}".format(full_url)
+        return []
+    list_of_cards = []
+    # [1:-1] cause first element is column legend names
+    # and the last is empty
+    base_url = "https://www.magiccardmarket.eu"
+    for row in range(len(soup.findAll('tr')))[1:-1]:
+        card_details = {}
+        # print "checking ",full_url
+        try:
+            thumb, expan, rarity, href, comment, singles, avail, price = soup.findAll(
+                'tr')[row].findAll('td')
+        except:
+            print "just one occurence for {}".format(full_url)
+            card_details = {
 
-                  "href":   full_url,
-                  "comment": "1 occurence",
-                  "singles": False,
-                  "avail": False,
-                  "expansion_set":"strange",
-                  "price": "strange"}
-      print "dupa2"
-      return [ card_details ]
-    if price.text == "N/A":
-      print "Warning! N/A for price {} .{}".format(price, href)
-      continue
-    if "Playmat" in str(href) :
-      print "Warning! This is Playmat , Skipping...", href
-      continue
-    if "Promo" in str(href) :
-      print "Warning! This is PROMO price , Skipping...", href
-      continue
+                "href":   full_url,
+                "comment": "1 occurence",
+                "singles": False,
+                "avail": False,
+                "expansion_set": "strange",
+                "price": "strange"}
+            print "dupa2"
+            return [card_details]
+        if price.text == "N/A":
+            print "Warning! N/A for price {} .{}".format(price, href)
+            continue
+        if "Playmat" in str(href):
+            print "Warning! This is Playmat , Skipping...", href
+            continue
+        if "Promo" in str(href):
+            print "Warning! This is PROMO price , Skipping...", href
+            continue
 
-    thumb = str(thumb).replace('<td>', '').replace('</td>', '')
-    expan = str(expan).replace('<td>', '').replace('</td>', '')
-    href = str(href).replace('<td>', '').replace('</td>', '')
-    comment = str(comment).replace('<td>', '').replace('</td>', '')
-    singles = str(singles).replace('<td>', '').replace('</td>', '')
-    avail = str(avail).replace('<td>', '').replace('</td>', '')
-    price = str(price).replace('<td>', '').replace('</td>', '').replace('&#x20AC;', 'EURO')
-    is_card_single = 'Products/Singles/' in href
-    if is_card_single:
-      expansion_set = href.split('Singles/')[1].split('/')[0]
-    else:
+        thumb = str(thumb).replace('<td>', '').replace('</td>', '')
+        expan = str(expan).replace('<td>', '').replace('</td>', '')
+        href = str(href).replace('<td>', '').replace('</td>', '')
+        comment = str(comment).replace('<td>', '').replace('</td>', '')
+        singles = str(singles).replace('<td>', '').replace('</td>', '')
+        avail = str(avail).replace('<td>', '').replace('</td>', '')
+        price = str(price).replace('<td>', '').replace(
+            '</td>', '').replace('&#x20AC;', 'EURO')
+        is_card_single = 'Products/Singles/' in href
+        if is_card_single:
+            expansion_set = href.split('Singles/')[1].split('/')[0]
+        else:
 
-      continue
-    if expansion_set in ["Alpha", "Beta", "Unlimited"]:
-      print "Expansion set is {} .skipping then ...for {}".format(expansion_set,
-                                                                  cardname)
-      continue
-    if "WCD" in expansion_set :
-      print "Expansion set is {} .skipping then ...HIS gold bordered {}".format(expansion_set,
-                                                                  cardname)
-      continue
-    # print "thumb", thumb
-    # print "expan", expan
-    # print "rarity", rarity
-    if comment:
-      print "comment", comment
-    # print "singles", singles
-    # print "avail", avail
+            continue
+        if expansion_set in ["Alpha", "Beta", "Unlimited"]:
+            print "Expansion set is {} .skipping then ...for {}".format(expansion_set,
+                                                                        cardname)
+            continue
+        if "WCD" in expansion_set:
+            print "Expansion set is {} .skipping then ...HIS gold bordered {}".format(expansion_set,
+                                                                                      cardname)
+            continue
+        # print "thumb", thumb
+        # print "expan", expan
+        # print "rarity", rarity
+        if comment:
+            print "comment", comment
+        # print "singles", singles
+        # print "avail", avail
 #    print "expansion_set", expansion_set
 #    print "price", price
 
-    card_details = {
-                  # "thumb":thumb,
-                  # "expan": expan,
-                  # "rarity": rarity,
-                  "href":   base_url + href.split("\"")[1],
-                  "comment": comment,
-                  "singles": singles,
-                  "avail": avail,
-                  "expansion_set":expansion_set,
-                  "price": price}
+        card_details = {
+            # "thumb":thumb,
+            # "expan": expan,
+            # "rarity": rarity,
+            "href":   base_url + href.split("\"")[1],
+            "comment": comment,
+            "singles": singles,
+            "avail": avail,
+            "expansion_set": expansion_set,
+            "price": price}
 
-    list_of_cards.append(card_details)
-  if list_of_cards == []	:
-    print "Nie ma!"
-  return list_of_cards
+        list_of_cards.append(card_details)
+    if list_of_cards == []	:
+        print "Nie ma!"
+    return list_of_cards
+
 
 def get_best_expansion(list_of_cards):
     """This is getting lowest price dict. the cheapest expansion set"""
-  #   for single_detail in list_of_cards:
-  #     print single_detail['expansion_set'] , " : ", single_detail['price']
+    #   for single_detail in list_of_cards:
+    #     print single_detail['expansion_set'] , " : ", single_detail['price']
     newlist_sorted_by_price = sorted(list_of_cards,
                                      key=lambda k: float(k['price'].
-                                     replace(",", ".").replace("EURO", "")))
-  #    for single_detail in newlist_sorted_by_price:
-  #      print single_detail['expansion_set'] , " : ", single_detail['price']
+                                                         replace(",", ".").replace("EURO", "")))
+    #    for single_detail in newlist_sorted_by_price:
+    #      print single_detail['expansion_set'] , " : ", single_detail['price']
     lowest_price_single = newlist_sorted_by_price[0]
     return lowest_price_single
 
-def get_price_trend(lowest_price_single):
-  """get price tend form sites like : 
-   https://www.magiccardmarket.eu/Products/Singles/Future+Sight/Tarmogoyf"""
-  # 'href': '<a href="/Products/Singles/Magic+2012/Manabarbs">Manabarbs</a>' -> /Products/Singles/Magic+2012/Manabarbs
-  url_for_single = lowest_price_single["href"]
-  final_single_url = url_for_single
-  price_trend = 0
-  try:
-    data_for_single = requests.get(final_single_url)
-    soup = BeautifulSoup(data_for_single.text)
-    # get table with details
-    availTable = soup.findAll("table", {"class":"availTable"})[0]
-    for row in availTable.findAll("tr"):
-      if "Price Trend" in row.text:
 
-        # print "Znalalzlem",row.text
-        price_trend = row.text.split("Price Trend:")[1].replace('&#x20AC;', 'EURO')
-        break
-  except:
-    print "problems for {}".format(final_single_url)
-  lowest_price_single['price_trend'] = price_trend
-  return  lowest_price_single
+def get_price_trend(lowest_price_single):
+    """get price tend form sites like :
+     https://www.magiccardmarket.eu/Products/Singles/Future+Sight/Tarmogoyf"""
+    # 'href': '<a href="/Products/Singles/Magic+2012/Manabarbs">Manabarbs</a>' -> /Products/Singles/Magic+2012/Manabarbs
+    url_for_single = lowest_price_single["href"]
+    final_single_url = url_for_single
+    price_trend = 0
+    try:
+        data_for_single = requests.get(final_single_url)
+        soup = BeautifulSoup(data_for_single.text)
+        # get table with details
+        availTable = soup.findAll("table", {"class": "availTable"})[0]
+        for row in availTable.findAll("tr"):
+            if "Price Trend" in row.text:
+
+                # print "Znalalzlem",row.text
+                price_trend = row.text.split("Price Trend:")[
+                    1].replace('&#x20AC;', 'EURO')
+                break
+    except:
+        print "problems for {}".format(final_single_url)
+    lowest_price_single['price_trend'] = price_trend
+    return lowest_price_single
+
 
 def get_price_and_set_MagicCardMarket(cardname):
-  """ function return dict with all details about card in form:
-  
-  {'comment': '',
-   'singles': 'Singles',
-   price_trend': u'113,50 EURO'
-   'price': '0,02 EURO',
-   'avail': '1379',
-   'href': 'https://www.magiccardmarket.eu/Products/Singles/Magic+2012/Manabarbs',
-   'expansion_set': 'Magic+2012'}
-  """
-  lowest_price_single = get_best_expansion(get_singles_details_MCM(cardname))
-  lowest_price_single = get_price_trend(lowest_price_single)
-  return lowest_price_single
+    """ function return dict with all details about card in form:
+
+    {'comment': '',
+     'singles': 'Singles',
+     price_trend': u'113,50 EURO'
+     'price': '0,02 EURO',
+     'avail': '1379',
+     'href': 'https://www.magiccardmarket.eu/Products/Singles/Magic+2012/Manabarbs',
+     'expansion_set': 'Magic+2012'}
+    """
+    lowest_price_single = get_best_expansion(get_singles_details_MCM(cardname))
+    lowest_price_single = get_price_trend(lowest_price_single)
+    return lowest_price_single
 
 if __name__ == "__main__":\
-  # print get_price_and_set_MagicCardMarket("tarmogoyf")
-  # print get_price_and_set_MagicCardMarket("doran, the siege tower")
-  # print get_price_and_set_MagicCardMarket("flooded strand")
-  print get_price_and_set_MagicCardMarket(" Fire-lit thicket")
-
+        # print get_price_and_set_MagicCardMarket("tarmogoyf")
+    # print get_price_and_set_MagicCardMarket("doran, the siege tower")
+    # print get_price_and_set_MagicCardMarket("flooded strand")
+    print get_price_and_set_MagicCardMarket(" Fire-lit thicket")
